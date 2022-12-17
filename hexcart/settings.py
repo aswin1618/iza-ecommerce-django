1a0a96cfac38b641b88f9070c869342b587889c2
Aswin <aswinsanjuuu@gmail.com>
2022-12-17 10:13:24 +0530

    migration files deleted

--- hexcart/settings.py
+++ hexcart/settings.py
@@ -88,8 +88,11 @@ AUTH_USER_MODEL = 'user.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': config('DATABASE_NAME'),
+        'USER' : config('DATABASE_USER'),
+        'PASSWORD' : config('DATABASE_PASSWORD'),
+        'HOST': 'localhost'
     }
 }
 

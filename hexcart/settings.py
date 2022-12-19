74d3466b94b9f49b3fcbfb20a893fc03365bd201
Aswin <aswinsanjuuu@gmail.com>
2022-12-19 15:39:59 +0530

    static files folder change

--- hexcart/settings.py
+++ hexcart/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 """
 
 from pathlib import Path
+import os
 from decouple import config
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -132,9 +133,9 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
 STATIC_URL = 'static/'
-STATIC_ROOT = BASE_DIR /'static'
+STATIC_ROOT = BASE_DIR / 'staticfiles'
 STATICFILES_DIRS = [
-    'hexcart/static',
+    os.path.join(BASE_DIR, 'static')
 ]
 
 # Default primary key field type
